7d822c71856637d0a45e21910251901f1d1c25ba
Aleksandra Nosińska <aleks.nosinska@gmail.com>
2024-08-12 16:46:44 +0200

    Modified repostories

--- src/main/java/com/aleksnose/hoteru/repository/TargetRoomRepository.java
+++ src/main/java/com/aleksnose/hoteru/repository/TargetRoomRepository.java
@@ -1,6 +1,7 @@
 package com.aleksnose.hoteru.repository;
 
 import com.aleksnose.hoteru.models.TargetRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface TargetRoomRepository extends JpaRepository<TargetRoom, Integer> {
+import org.springframework.data.repository.CrudRepository;
+
+public interface TargetRoomRepository extends CrudRepository<TargetRoom, Integer> {
 }
--- src/main/java/com/aleksnose/hoteru/repository/UserRepository.java
+++ src/main/java/com/aleksnose/hoteru/repository/UserRepository.java
@@ -3,21 +3,16 @@ package com.aleksnose.hoteru.repository;
 import com.aleksnose.hoteru.models.User;
 import com.aleksnose.hoteru.models.WorkerInHotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    default User findByNameAndSurname(String name, String surname) {
-        var users = findAll();
-        var foundUser = users.stream()
-                .filter(user -> user.getName().equals(name) && user.getSurname().equals(surname))
-                .findAny()
-                .orElseThrow(() -> null);
+    //User findByNameAndSurname(String name, String surname);
 
-        return foundUser;
-    }
-
+    //@Query("select u from User u where u.Name = ?1 and u.Surname = ?2")
+    List<User> findByNameAndSurname(String name, String surname);
 }
